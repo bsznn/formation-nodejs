5cb50cf8a8f00f1a674541f6599d49976ede8165
--replace-all <bulgan.suzan@gmail.com>
2023-03-01 11:39:18 +0100

    Ajout page student

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,34 @@ newApp.listen({ port: 4646, host: '127.0.0.1' }, () => {
   console.log('Mon serveur est prèt : http://127.0.0.1:4646')
 })
 
+newApp.get("/student", (request, response) => {
+
+  response.header("Developed-With", "fastify");
+  return [
+    {
+      id: "1",
+      nom: "john",
+      prenom: "john",
+      age: "32",
+    },
+    {
+      id: "2",
+      nom: "john",
+      prenom: "rose",
+      age: "36",
+    },
+    {
+      id: "3",
+      nom: "john",
+      prenom: "jane",
+      age: "40",
+    },
+    {
+      id: "4",
+      nom: "john",
+      prenom: "jean",
+      age: "38",
+    },
+  ];
+});
+
